d23e22eb3b4bdb97bbf58039c4732975dd0c0b79
nguyen280795 <nguyenpham.280795@gmail.com>
2017-04-08 09:27:56 +0700

    update

--- gamemonster/js/gamemonter.js
+++ gamemonster/js/gamemonter.js
@@ -6,21 +6,35 @@ var bodyCtx = bodyCanvas.getContext("2d");
 
 var lastUpdateTime = Date.now();
 
+var windows = window;
+requestAnimationFrame = windows.requestAnimationFrame || windows.webkitRequestAnimationFrame || windows.msRequestAnimationFrame || windows.mozRequestAnimationFrame;
+
 var FPS = 144;
 var TICKS = 1000 / FPS;
 
 var scoreNumber = 0;
+var highScore = 0;
 var heartNumber = 5;
-var speedNumber = 2;
+var speedNumber = 1;
 var monsterNumber = 1;
 var boomNumber = 3;
 var stopNumber = 3;
+
+var boolStop = false;
+var boolPause = false;
+var boolBoom = true;
+var startgame = true;
+
+if (typeof(Storage) !== "undefined")
+    localStorage.setItem('highScore', highScore);
 
 // var myArray = [0, 210, 420];
 // var rand1 = myArray[Math.floor(Math.random() * myArray.length)];
 // var rand2 = myArray[Math.floor(Math.random() * myArray.length)];
 // var rand3 = rand1;
 // var rand4 = rand2;
+
+var listBlood = new Array();
 
 //===================>Start create img<===================//
 var heartImage = new Image();
@@ -51,6 +65,8 @@ monsterImageSize = {
 var bgImage = new Image();
 bgImage.src = "img/background.jpg";
 //===================>End create img<===================//
+
+//===================>Start create prototype monster<===================//
 function Monster(beginX, beginY, endX, endY, startX, startY, stopX, stopY, speed, show, click, dieX, dieY) {
     this.beginX = beginX;
     this.beginY = beginY;
@@ -66,14 +82,17 @@ function Monster(beginX, beginY, endX, endY, startX, startY, stopX, stopY, speed
     this.dieX = dieX;
     this.dieY = dieY;
 }
-var Monster1 = new Monster(0, 0, 210, 210, 0, 0, 210, 210, speedNumber, true, false, 0, 0);
-var Monster2 = new Monster(210, 0, 210, 210, 210, 0, 210, 210, speedNumber, false, false, 0, 0);
-var Monster3 = new Monster(420, 0, 210, 210, 420, 0, 210, 210, speedNumber, false, false, 0, 0);
-var Monster4 = new Monster(420, 210, 210, 210, 420, 210, 210, 210, speedNumber, false, false, 0, 0);
-var Monster5 = new Monster(420, 420, 210, 210, 420, 420, 210, 210, speedNumber, false, false, 0, 0);
-var Monster6 = new Monster(210, 420, 210, 210, 210, 420, 210, 210, speedNumber, false, false, 0, 0);
-var Monster7 = new Monster(0, 420, 210, 210, 0, 420, 210, 210, speedNumber, false, false, 0, 0);
-var Monster8 = new Monster(0, 210, 210, 210, 0, 210, 210, 210, speedNumber, false, false, 0, 0);
+//===================>End create prototype monster<===================//
+
+//===================>Start create monster<===================//
+var Monster1 = new Monster(0, 0, 120, 120, 0, 0, 120, 120, speedNumber, false, false, 0, 0);
+var Monster2 = new Monster(210, 0, 210, 120, 210, 0, 210, 120, speedNumber, false, false, 0, 0);
+var Monster3 = new Monster(420, 0, 300, 120, 420, 0, 300, 120, speedNumber, false, false, 0, 0);
+var Monster4 = new Monster(420, 210, 300, 210, 420, 210, 300, 210, speedNumber, false, false, 0, 0);
+var Monster5 = new Monster(420, 420, 300, 300, 420, 420, 300, 300, speedNumber, false, false, 0, 0);
+var Monster6 = new Monster(210, 420, 210, 300, 210, 420, 210, 300, speedNumber, false, false, 0, 0);
+var Monster7 = new Monster(0, 420, 120, 300, 0, 420, 120, 300, speedNumber, false, false, 0, 0);
+var Monster8 = new Monster(0, 210, 120, 210, 0, 210, 120, 210, speedNumber, false, false, 0, 0);
 // if (rand1 === 210 && rand2 === 210) {
 //     var Monster9 = new Monster(0, 0, 210, 210, 0, 0, 210, 210, speedNumber, false, false, 0, 0);
 //     console.log("else 9");
@@ -82,7 +101,7 @@ var Monster8 = new Monster(0, 210, 210, 210, 0, 210, 210, 210, speedNumber, fals
 //     Monster9 = new Monster(rand3, rand4, 210, 210, rand3, rand4, 210, 210, speedNumber, false, false, 0, 0);
 //     console.log("else 9");
 // }
-
+//===================>End create monster<===================//
 
 //===================>Start load screen game<===================//
 function loadScreen() {
@@ -110,6 +129,8 @@ function loadScreen() {
     bodyCtx.clearRect(0, 0, bodyCanvas.width, bodyCanvas.height);
     bodyCtx.drawImage(bgImage, 0, 0, bodyCanvas.width, bodyCanvas.height);
 
+    updateBlood();
+
     if (Monster1.show)
         bodyCtx.drawImage(monsterImage, Monster1.startX, Monster1.startY, monsterImageSize.width, monsterImageSize.height);
     if (Monster2.show)
@@ -129,9 +150,12 @@ function loadScreen() {
     // if (Monster9.show)
     //     bodyCtx.drawImage(monsterImage, Monster9.startX, Monster9.startY, monsterImageSize.width, monsterImageSize.height);
 
+    if (boolPause)
+        SetDefalult("PAUSE!!");
 }
 //===================>End load screen game<===================//
 
+//===================>Start create function refresh<===================//
 function refreshMonster(monster) {
     monster.show = false;
     monster.startX = monster.beginX;
@@ -140,7 +164,9 @@ function refreshMonster(monster) {
     monster.stopY = monster.endY;
     monster.speed = speedNumber;
 }
+//===================>End create function refresh<===================//
 
+//===================>Start create function randon monster<===================//
 function randomMonster() {
     if (!Monster1.show)
         refreshMonster(Monster1);
@@ -161,7 +187,6 @@ function randomMonster() {
     // if (!Monster9.show)
     //     refreshMonster(Monster8);
     var value = Math.floor((Math.random() * 8) + 1);
-    console.log(value);
     switch (value) {
         case 1:
             if (!Monster1.show)
@@ -201,40 +226,175 @@ function randomMonster() {
         //     break;
     }
 }
+//===================>End create function random monster<===================//
 
+//===================>Start create function monster move<===================//
 function updateMonster(monster) {
-
-    // monster.click = true;
+    monster.click = true;
     if (monster.startX === monster.stopX && monster.startY === monster.stopY) {
         monster.startX = monster.stopX;
         monster.startY = monster.stopY;
         monster.stopX = monster.beginX;
         monster.stopY = monster.beginY;
     } else {
-        if (monster.startX < monster.stopX) {
-            monster.startX += monster.speed;
-        }
-        else {
-            monster.startX -= monster.speed;
+        if (monster.startX === monster.stopX) {
 
+        } else {
+            if (monster.startX < monster.stopX) {
+                monster.startX += monster.speed;
+            }
+            else {
+                monster.startX -= monster.speed;
+
+            }
         }
-        if (monster.startY < monster.stopY) {
-            monster.startY += monster.speed;
-        }
-        else {
-            monster.startY -= monster.speed;
+        if (monster.startY === monster.stopY) {
+
+        } else {
+            if (monster.startY < monster.stopY) {
+                monster.startY += monster.speed;
+            }
+            else {
+                monster.startY -= monster.speed;
+            }
         }
     }
     if (monster.startX === monster.beginX && monster.startY === monster.beginY) {
         monster.show = false;
+        if (heartNumber > 0 && scoreNumber === 0) {
+            heartNumber -= 1;
+        }
+        else {
+            heartNumber -= 1;
+            scoreNumber -= 10;
+        }
         randomMonster();
     }
 }
+//===================>End create function monster move<===================//
+
+//===================>Start create event click body<===================//
+bodyCanvas.addEventListener("click", function (e) {
+    if (boolPause === false) {
+        var cursorX = e.pageX - this.offsetLeft;
+        var cursorY = e.pageY - this.offsetTop;
+    }
+
+    if (startgame === false && boolStop === false) {
+        cursorX = 0;
+        cursorY = 0;
+    }
+
+    if (boolPause === false) {
+        if (Monster1.show)
+            clickMonster(Monster1, cursorX, cursorY);
+        if (Monster2.show)
+            clickMonster(Monster2, cursorX, cursorY);
+        if (Monster3.show)
+            clickMonster(Monster3, cursorX, cursorY);
+        if (Monster4.show)
+            clickMonster(Monster4, cursorX, cursorY);
+        if (Monster5.show)
+            clickMonster(Monster5, cursorX, cursorY);
+        if (Monster6.show)
+            clickMonster(Monster6, cursorX, cursorY);
+        if (Monster7.show)
+            clickMonster(Monster7, cursorX, cursorY);
+        if (Monster8.show)
+            clickMonster(Monster8, cursorX, cursorY);
+    }
+
+    if (boolStop) {
+        if (Monster1.show)
+            executeActionStop(Monster1, cursorX, cursorY);
+        if (Monster2.show)
+            executeActionStop(Monster2, cursorX, cursorY);
+        if (Monster3.show)
+            executeActionStop(Monster3, cursorX, cursorY);
+        if (Monster4.show)
+            executeActionStop(Monster4, cursorX, cursorY);
+        if (Monster5.show)
+            executeActionStop(Monster5, cursorX, cursorY);
+        if (Monster6.show)
+            executeActionStop(Monster6, cursorX, cursorY);
+        if (Monster7.show)
+            executeActionStop(Monster7, cursorX, cursorY);
+        if (Monster8.show)
+            executeActionStop(Monster8, cursorX, cursorY);
+        // if (MonsterNine.show)
+        //     executeActionStop(MonsterNine, cursorX, cursorY);
+        loadScreen();
+    }
+    if (heartNumber <= 0) {
+        SetDefalult_GameOver();
+    }
+});
+//===================>End create event click body<===================//
+
+//===================>Start create function click monster<===================//
+function clickMonster(monster, cursorX, cursorY) {
+    if (monster.click) {
+        if (cursorX >= monster.startX && cursorX <= monster.startX + monsterImageSize.width && cursorY >= monster.startY && cursorY <= monster.startY + monsterImageSize.height) {
+            scoreNumber += 10;
+            addHeart();
+            monster.click = false;
+            monster.show = false;
+            monster.dieX = monster.startX;
+            monster.dieY = monster.startY;
+            addElementBlood(monster.dieX, monster.dieY);
+            for (var i = 0; i < monsterNumber; i++)
+                randomMonster();
+        }
+        else {
+            // if (boolPause === false) {
+            //     heartNumber -= 1;
+            //     scoreNumber -= 10;
+            //     // if (heartNumber > 0 && scoreNumber === 0) {
+            //     //     // heartNumber -= 1;
+            //     //
+            //     //     console.log("aaa");
+            //     // }
+            //     // else {
+            //     //     // heartNumber -= 1;
+            //     //     console.log("bbb");
+            //     //     scoreNumber -= 10;
+            //     //     if (scoreNumber <= 0) {
+            //     //         scoreNumber = 0;
+            //     //     }
+            //     // }
+            // }
+        }
+    }
+}
+//===================>End create function click monster<===================//
+
+//===================>Start create function add blood<===================//
+function addElementBlood(initX, initY) {
+    Blood = {
+        initX: initX,
+        initY: initY
+    };
+    listBlood[listBlood.length] = Blood;
+}
+//===================>End create function add blood<===================//
+
+//===================>Start create function update blood<===================//
+function updateBlood() {
+    if (listBlood.length > 0) {
+        for (var i = 0; i < listBlood.length; i++) {
+            bodyCtx.drawImage(bloodImage, listBlood[i].initX, listBlood[i].initY)
+        }
+    }
+}
+//===================>End create function updateb lood<===================//
+
+//===================>Start create function main<===================//
 function main() {
     var NOW = Date.now();
     var differentTime = NOW - lastUpdateTime;
     if (differentTime >= TICKS) {
         loadScreen();
+        createLevel();
         if (Monster1.show)
             updateMonster(Monster1);
         if (Monster2.show)
@@ -254,8 +414,230 @@ function main() {
         // if (Monster9.show)
         //     updateMonster(Monster9);
     }
-    requestAnimationFrame(main);
+    if (heartNumber <= 0) {
+        var temp = parseInt(localStorage.getItem('highScore'));
+        if (temp < scoreNumber)
+            localStorage.setItem('highScore', scoreNumber);
+        SetDefalult_GameOver();
+    }
+    if (startgame)
+        requestAnimationFrame(main);
 }
-var windows = window;
-requestAnimationFrame = windows.requestAnimationFrame || windows.webkitRequestAnimationFrame || windows.msRequestAnimationFrame || windows.mozRequestAnimationFrame;
+//===================>End create function main<===================//
+
+//===================>Start create function level<===================//
+function createLevel() {
+    var level = Math.floor(scoreNumber / 100);
+    switch (level) {
+        case 1:
+            speedNumber = 1;
+            monsterNumber = 1;
+            break;
+        case 2:
+            speedNumber = 1;
+            monsterNumber = 3;
+            break;
+        case 3:
+            speedNumber = 2;
+            monsterNumber = 4;
+            break;
+        case 4:
+            speedNumber = 2;
+            monsterNumber = 5;
+            break;
+        case 5:
+            speedNumber = 3;
+            monsterNumber = 5;
+            break;
+        case 6:
+            speedNumber = 3;
+            monsterNumber = 6;
+            break;
+        case 7:
+            speedNumber = 4;
+            monsterNumber = 7;
+            break;
+        case 8:
+            speedNumber = 4;
+            monsterNumber = 8;
+            break;
+    }
+}
+//===================>End create function level<===================//
+
+//===================>Start create function GameOver<===================//
+function SetDefalult_GameOver() {
+    startgame = false;
+
+    bodyCtx.fillStyle = '#FFFFFF';
+    bodyCtx.font = '40px Arial';
+    bodyCtx.fillText('Game Over!!', 130, 200);
+
+    bodyCtx.fillStyle = '#5bfa3f';
+    bodyCtx.font = '20px Arial';
+    bodyCtx.fillText('Score: ' + scoreNumber, 130, 240);
+    bodyCtx.fillText('High Score: ' + localStorage.getItem('highScore'), 130, 280);
+}
+//===================>End create function GameOver<===================//
+
+//===================>Start create function add heart<===================//
+function addHeart() {
+    if (scoreNumber !== 0 && scoreNumber % 100 === 0 && heartNumber < 5) {
+        heartNumber++;
+    }
+}
+//===================>End create function add heart<===================//
+
+//===================>Start event click header <===================//
+headerCanvas.addEventListener('click', function (e) {
+    var cursorX = e.pageX - this.offsetLeft;
+    var cursorY = e.pageY - this.offsetTop;
+
+    //Button Restart
+    if (cursorX > 450 && cursorX < 490 && cursorY > 60 && cursorY < 100) {
+        bodyCtx.clearRect(0, 0, bodyCtx.width, bodyCtx.height);
+        restart();
+
+    }
+
+    //fix event pause
+    if (heartNumber <= 0 || boolStop) {
+        cursorX = 0;
+        cursorY = 0;
+    }
+
+    // Button Boom
+    if (cursorX > 290 && cursorX < 340 && cursorY > 60 && cursorY < 100) {
+        if (boolBoom) {
+            executeActionBoom();
+            boomNumber--;
+            if (boomNumber <= 0) {
+                boolBoom = false;
+            }
+        }
+    }
+
+    //Button Stop
+    if (cursorX > 350 && cursorX < 390 && cursorY > 60 && cursorY < 100) {
+        boolPause = false;
+        if (stopNumber > 0) {
+            if (startgame) {
+                startgame = false;
+                boolStop = true;
+                stopNumber--;
+            }
+            setTimeout(function () {
+                startgame = true;
+                main();
+                boolStop = false;
+            }, 3000)
+        }
+    }
+
+    //Button Pause
+    if (cursorX > 400 && cursorX < 440 && cursorY > 60 && cursorY < 100) {
+        boolStop = false;
+        if (startgame) {
+            startgame = false;
+            boolPause = true;
+        } else {
+            startgame = true;
+            boolPause = false;
+            main();
+        }
+    }
+});
+//===================>End event click header <===================//
+
+//===================>Start function restart <===================//
+function restart() {
+    if (boolPause || startgame === false) {
+        load();
+        main();
+    }
+    else {
+        load();
+    }
+}
+
+function load() {
+    speedNumber = 1;
+    scoreNumber = 0;
+    monsterNumber = 1;
+    heartNumber = 5;
+    boomNumber = 3;
+    stopNumber = 3;
+    boolPause = false;
+    boolBoom = true;
+    boolStop = false;
+    startgame = true;
+    listBlood = new Array();
+    refreshMonster(Monster1);
+    refreshMonster(Monster2);
+    refreshMonster(Monster3);
+    refreshMonster(Monster4);
+    refreshMonster(Monster5);
+    refreshMonster(Monster6);
+    refreshMonster(Monster7);
+    refreshMonster(Monster8);
+    randomMonster();
+}
+//===================>End function restart <===================//
+
+//===================>Start function show info <===================//
+function SetDefalult(string) {
+    bodyCtx.fillStyle = '#FFFFFF';
+    bodyCtx.font = '50px Arial';
+    bodyCtx.fillText(string, 180, 240);
+}
+//===================>End function show info <===================//
+
+//===================>Start function executeActionBoom <===================//
+function executeActionBoom() {
+    if (Monster1.show)
+        boomForMonster(Monster1);
+    if (Monster2.show)
+        boomForMonster(Monster2);
+    if (Monster3.show)
+        boomForMonster(Monster3);
+    if (Monster4.show)
+        boomForMonster(Monster4);
+    if (Monster5.show)
+        boomForMonster(Monster5);
+    if (Monster6.show)
+        boomForMonster(Monster6);
+    if (Monster7.show)
+        boomForMonster(Monster7);
+    if (Monster8.show)
+        boomForMonster(Monster8);
+    // if (MonsterNine.show)
+    //     boomForMonster(MonsterNine);
+    for (var i = 0; i < monsterNumber; i++) {
+        randomMonster();
+    }
+}
+//===================>End function executeActionBoom <===================//
+
+//===================>Start function boomForMonster <===================//
+function boomForMonster(monster) {
+    scoreNumber += 10;
+    monster.show = false;
+    monster.click = false;
+    addElementBlood(monster.startX, monster.startY);
+}
+//===================>End function boomForMonster <===================//
+
+//===================>Start function executeActionStop <===================//
+function executeActionStop(monster, cursorX, cursorY) {
+    if (monster.click) {
+        if (cursorX > monster.startX && cursorX < monster.startX + monsterImageSize.width && cursorY > monster.startY && cursorY < monster.startY + monsterImageSize.height) {
+            scoreNumber += 10;
+            monster.click = false;
+            monster.show = false;
+        }
+    }
+}
+//===================>Start function executeActionStop <===================//
+
 main();
+randomMonster();
